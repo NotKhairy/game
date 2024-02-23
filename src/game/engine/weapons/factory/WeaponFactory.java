d17521c8074d2c862ddaf38e86c5413b31602978
Seif <seifeldinhatem@gmail.com>
2024-02-23 23:48:26 +0200

    day 1

---
+++ src/game/engine/weapons/factory/WeaponFactory.java
@@ -0,0 +1,11 @@
+package game.engine.weapons.factory;
+
+public class WeaponFactory {
+	private HashMap<Integer, WeaponRegistry> weaponShop;
+
+	
+	
+	WeaponFactory() throws IOException:
+		this.weaponShop= weaponShop;
+	
+}
